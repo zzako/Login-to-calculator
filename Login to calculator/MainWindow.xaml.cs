269802e06879adeb2eada282a7cf69456115471a
Zaakir Ismail <zakmini.isi@gmail.com>
2020-09-25 16:52:58 +0200

    Add project files.

---
+++ Login to calculator/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login_to_calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            string name = "user";
+            string password = "user123";
+            string inputName = TextBox1.Text;
+            string inputPassword = TextBox2.Text;
+            Window window = new Window();
+            if (inputName == name && inputPassword == password)
+            {
+                MainWindow mainwindow = new MainWindow();
+                mainwindow.Hide();
+                Window1 window1 = new Window1();
+                window1.Show();
+
+                
+            }
+            else if (inputName != name || inputPassword != password)
+            {
+                MessageBox.Show("Incorrect username or password");
+            }
+        }
+    }
+}
---
+++ Login to calculator/Window1.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Login_to_calculator
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double f = Convert.ToDouble(TextBox1.Text);
+                double c;
+                c = (f - 32) * 5 / 9;
+                AnswerL.Content = c.ToString() + "C";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double c = Convert.ToDouble(TextBox1.Text);
+                double f;
+                f = c * 1.8 + 32;
+                AnswerL.Content = f.ToString() + "F";
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+    }
+    
+}
